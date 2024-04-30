51e6bdd3a13072a723e010380a41565712839975
Mohamed ElGanzory <mohamedelganzory.dev@gmail.com>
2024-04-30 12:49:09 +0300

    Main structure and Styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./index.html"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        sm: "370px",
+        md: "505px"
+      },
+      boxShadow: {
+        "green": '0 0 30px 5px hsl(150, 100%, 66%)'
+      }
+    },
   },
   plugins: [],
 }
