d90a0cb2f0e34c207a88e5ca937375912b7040f8
Mohamed ElGanzory <mohamedelganzory.dev@gmail.com>
2024-04-30 13:55:08 +0300

    Responsive Changes and Main Roll Functionality.

---
+++ main.js
@@ -0,0 +1,21 @@
+let id = 0;
+let advice = "";
+
+function roll() {
+const apiUrl = "https://api.adviceslip.com/advice";
+
+fetch(apiUrl).then(response => {
+    if (!response.ok) {
+        throw new Error('Resposne Failed')
+    }
+    return response.json()
+}).then(data => {
+    console.log(data)
+    id = data["slip"]["id"]
+    advice = data["slip"]["advice"]
+    document.querySelector("#id").innerHTML = id;
+    document.querySelector("#advice").innerHTML = advice;
+}).catch(error => {
+    console.log("Error:", error)
+})
+}
